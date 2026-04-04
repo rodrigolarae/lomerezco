3fd0300444214cdeb336a0da53ef19000ff4411a
rodrigolarae <rodrigo.larae@gmail.com>
2026-04-04 16:16:21 -0300

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,5 +1,5 @@
 // api/verify.js
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
